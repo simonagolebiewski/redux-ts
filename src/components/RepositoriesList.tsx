fe520e345ea23f72cf1322342ae8e80f4c706cb9
Simona Golebiewski <simona.golebi@gmail.com>
2021-04-08 12:31:40 +0200

    display list of packages when user searches

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -23,7 +23,8 @@ const RepositoriesList: React.FC = () => {
             </form>
             {error && <h3>{error}</h3>}
             {loading && <h3>Loading...</h3>}
-            {!error && !loading && data}
+            {!error && !loading && 
+            data.map((name) => <div key={name}>{name}</div>)}
         </div>
     );
 };
